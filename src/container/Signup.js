7adfa1ac218c1683d6dd1d69c7935054e72305e1
Stephen Zheng <65367318+i-le@users.noreply.github.com>
2020-07-10 11:01:23 -0400

    Add files via upload
    add something

---
+++ src/container/Signup.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class Signup extends React.Component {
+
+    render() {
+        return (
+            <div className="login-from">
+                <h2>Sign In</h2>
+                <div className="form-input">
+                    <input type="text" name="" placeholder="Email" />
+                    <div className="form-input">
+                    <input type="password" name="" placeholder="Password" />
+                <div className="form-input">
+                    <input type="password" name="" placeholder="Confirm" />
+                </div>
+                 </div>
+                </div>
+                </div>
+
+    
+        )
+    }
+}
